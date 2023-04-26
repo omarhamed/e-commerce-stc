0a8936c036eadf49f9122887588f34d40ace0a9d
omarhamed <oh83521@gmail.com>
2023-04-26 17:55:00 +0200

    E-commerce Challange

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,45 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product/product.component';
+import { LoginComponent } from './auth/login/login.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { ProductNewComponent } from './components/product-new/product-new.component';
+import { ProductGuard } from './services/product.guard';
+import { LogoutComponent } from './auth/logout/logout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'products',
+    component: ProductComponent,
+    canActivate: [ProductGuard],
+  },
+  {
+    path: 'products/new',
+    component: ProductNewComponent,
+    canActivate: [ProductGuard],
+  },
+  {
+    path: 'products/:id',
+    component: ProductDetailsComponent,
+    canActivate: [ProductGuard],
+  },
+  {
+    path: 'products/category/:category',
+    component: ProductComponent,
+    canActivate: [ProductGuard],
+  },
+  {
+    path: 'logout',
+    component: LogoutComponent,
+    canActivate: [ProductGuard],
+  },
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '**', redirectTo: '/products', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { ProductComponent } from './components/product/product.component';
+import { LoginComponent } from './auth/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { ProductNewComponent } from './components/product-new/product-new.component';
+import { LogoutComponent } from './auth/logout/logout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    LoginComponent,
+    ProductCategoryMenuComponent,
+    ProductDetailsComponent,
+    ProductNewComponent,
+    LogoutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MaterialModule,
+    NgbModule,
+    FormsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
